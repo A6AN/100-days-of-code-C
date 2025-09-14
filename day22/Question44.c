eea481f968c1dee9b59ee0f08717b5348eebb0ce
Ayan Pande <46679573+A6AN@users.noreply.github.com>
2025-09-14 17:46:32 +0530

    Day 22

---
+++ day22/Question44.c
@@ -0,0 +1,18 @@
+//Write a program to find the sum of the series: 1 + 3/4 + 5/6 + 7/8 + … up to n terms.
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    float sum = 1.0;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        sum += (2 * i + 1) / (float)(2 * i + 2);
+    }
+
+    printf("The sum of the series is: %.2f\n", sum);
+    return 0;
+}
