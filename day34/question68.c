76056e2ebb46aa6414e46323075a42344e40bb7c
Ayan Pande <46679573+A6AN@users.noreply.github.com>
2025-11-11 19:49:40 +0530

    Day 34

---
+++ day34/question68.c
@@ -0,0 +1,35 @@
+//Delete an element from an array.
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, element;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to delete: ");
+    scanf("%d", &element);
+
+    for (i = 0; i < n; i++) {
+        if (arr[i] == element) {
+            for (int j = i; j < n - 1; j++) {
+                arr[j] = arr[j + 1];
+            }
+            n--; 
+            i--;  
+        }
+    }
+
+    printf("Array after deletion:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
